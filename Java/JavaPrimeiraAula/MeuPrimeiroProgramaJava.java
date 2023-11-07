5d5d5bf34b3a3040ee610f320f45ad82d590c341
pwd.Louis <149837224+pwdLuis@users.noreply.github.com>
2023-11-07 18:34:37 -0300

    Add files via upload

---
+++ Java/JavaPrimeiraAula/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,32 @@
+/* Salvo como  "Meu primeiro progama Java" -> MeuPrimeiroProgramaJava.java
+*/
+
+
+/* Nome da classe: */
+class MeuPrimeiroProgramaJava
+{
+
+    /* Módulo principal com a entrada pela linha de comando */
+    public static void main(String entrada[])
+    {
+
+        /* Declaração das variáveis */
+        int inteiro = 19;
+        char caracter = 'M';
+        String mf = "Masculino";
+        double real = 1.85;
+        String frase = "Louis.PWD ";
+        boolean VF = true;
+
+        if (VF == true) 
+        {
+
+            System.out.println("Eu sou o " + frase + "e tenho " + inteiro + " anos, e tambem " + real + "m de altura. \nMeu genero biológico : " + mf + " - " + caracter);
+
+        }
+     
+        System.exit(0);
+
+    }
+
+}
